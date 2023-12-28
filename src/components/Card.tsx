7065d7cfb382d42e5d39b2db51f54b475dc08440
Maxime Deroullers <maxime.deroullers@gmail.com>
2023-12-28 09:26:36 +0100

    init

---
+++ src/components/Card.tsx
@@ -0,0 +1,35 @@
+import { slugifyStr } from "@utils/slugify";
+import Datetime from "./Datetime";
+import type { CollectionEntry } from "astro:content";
+
+export interface Props {
+  href?: string;
+  frontmatter: CollectionEntry<"blog">["data"];
+  secHeading?: boolean;
+}
+
+export default function Card({ href, frontmatter, secHeading = true }: Props) {
+  const { title, pubDatetime, modDatetime, description } = frontmatter;
+
+  const headerProps = {
+    style: { viewTransitionName: slugifyStr(title) },
+    className: "text-lg font-medium link-underline",
+  };
+
+  return (
+    <li className="my-6">
+      <a
+        href={href}
+        className="link-underline inline-block text-lg font-medium text-skin-accent"
+      >
+        {secHeading ? (
+          <h2 {...headerProps}>{title}</h2>
+        ) : (
+          <h3 {...headerProps}>{title}</h3>
+        )}
+      </a>
+      <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
+      <p>{description}</p>
+    </li>
+  );
+}
