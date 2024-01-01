3bbb1e3e47e1741db7556464d92bc489784eb8a7
Maxime Deroullers <maxime.deroullers@gmail.com>
2024-01-01 21:03:57 +0000

    feat: add anchors to titles

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import h from "hastscript";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
@@ -23,6 +26,52 @@ export default defineConfig({
         remarkCollapse,
         {
           test: "Table of contents",
+        },
+      ],
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          properties: {
+            className: [
+              "text-violet-500",
+              "font-semibold",
+              "rounded",
+              "focus:outline-blue-500",
+              "focus:outline-1",
+              "focus:outline-offset-4",
+              "group",
+              "heading-anchor",
+            ],
+          },
+          group: {
+            type: "element",
+            tagName: "div",
+            properties: {
+              className: [
+                "absolute",
+                "-left-2",
+                "transform",
+                "-translate-x-full",
+              ],
+            },
+          },
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: {
+              className: ["link-underline"],
+            },
+            children: [
+              {
+                type: "text",
+                value: "#",
+              },
+            ],
+          },
         },
       ],
     ],
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://mderoullers.com", // replace this with your deployed domain
   author: "Maxime Deroullers",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  desc: "Thoughts on Cloud/DevOps, IT conferences, IT management, and other interesting topics. Welcome aboard!",
   title: "Blog - Maxime Deroullers",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
@@ -12,7 +12,7 @@ export const SITE: Site = {
 
 export const LOCALE = {
   lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  langTag: ["en-EN", "fr-FR"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
